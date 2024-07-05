a216febed3cc748f332a7e8cd2768cacecc1db36
Matthew Wilding <mbwilding@gmail.com>
2024-07-06 03:37:07 +0800

    init

---
+++ Formula/neospleen-nerd-font.rb
@@ -0,0 +1,12 @@
+class NeospleenNerdFont < Formula
+    version "1.0.44"
+    desc "A modern monospaced font inspired by Spleen - Nerd Font"
+    homepage "https://github.com/mbwilding/NeoSpleen"
+    url "https://github.com/mbwilding/NeoSpleen/releases/download/#{version}/NeoSpleen-NerdFont.ttf"
+    sha256 "4d33bc7b27e1d87632c6d151677ae030fb3c6f5007692556f043f888505fa391"
+    license "BSD-2-Clause"
+
+    def install
+        (share/"fonts").install "NeoSpleen-NerdFont.ttf"
+    end
+end
---
+++ Formula/neospleen.rb
@@ -0,0 +1,12 @@
+class Neospleen < Formula
+    version "1.0.44"
+    desc "A modern monospaced font inspired by Spleen"
+    homepage "https://github.com/mbwilding/NeoSpleen"
+    url "https://github.com/mbwilding/NeoSpleen/releases/download/#{version}/NeoSpleen.ttf"
+    sha256 "01d9e7f332ae4d0390e3f230e711fde44904d39965c310ab9319c1e5fcf1e1b4"
+    license "BSD-2-Clause"
+
+    def install
+        (share/"fonts").install "NeoSpleen.ttf"
+    end
+end
